b4f1f07783bf6ef0d9b75c22c33786927e282810
lhn136 <32319480+lhn136@users.noreply.github.com>
2017-10-04 11:27:34 -0700

    indent

--- Project 1/src/hello.java
+++ Project 1/src/hello.java
@@ -3,7 +3,7 @@ public class hello //public class hello
 {
 	public static void main(String[] args) //utilize main to execute the program
 	{
-	System.out.println("Hello, Holly");	 //"Hello, Holly" is printed out into the console
+		System.out.println("Hello, Holly");	 //"Hello, Holly" is printed out into the console
 	}
 	
 }
