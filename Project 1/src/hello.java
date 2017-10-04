ec627f108d0299211960425eb04dadd008344282
lhn136 <32319480+lhn136@users.noreply.github.com>
2017-10-04 11:24:08 -0700

    added folders

---
+++ Project 1/src/hello.java
@@ -0,0 +1,9 @@
+
+public class hello //public class hello
+{
+	public static void main(String[] args) //utilize main to execute the program
+	{
+	System.out.println("Hello, Holly");	 //"Hello, Holly" is printed out into the console
+	}
+	
+}
